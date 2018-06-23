0d9eafc909d06e60e970915ef7540aa24d78d3c2
Groutch <hurizen1@hotmail.com>
2018-06-23 19:02:45 +0200

    fix DB structure

--- DoubleBDD.sql
+++ DoubleBDD.sql
@@ -1,19 +1,20 @@
 CREATE TABLE Post(
 	id_Post INT NOT NULL AUTO_INCREMENT,
     titre VARCHAR(45),
-    corps VARCHAR(500),
+    corps VARCHAR(10000),
     date_Post DATE,
     id_User INT,
 	PRIMARY KEY(id_Post)
 );
 
-CREATE TABLE Commentary(
-    id_Commentary INT NOT NULL AUTO_INCREMENT,
-    corps_commentaires VARCHAR (500),
-    id_User INT,
-    date_Commentary DATE,
+CREATE TABLE Commentaire(
+    id_Commentaire INT NOT NULL AUTO_INCREMENT,
+    corps_Commentaire VARCHAR (500),
+    date_Commentaire DATE,
     id_Post INT,
-    PRIMARY KEY(id_Commentary)
+    id_User INT,
+    PRIMARY KEY(id_Commentaire)
 );
 
-ALTER TABLE Commentary ADD CONSTRAINT FOREIGN KEY (id_Post) REFERENCES Post (id_Post);
+ALTER TABLE Commentaire ADD CONSTRAINT FOREIGN KEY (id_Post) REFERENCES Post (id_Post);
+
